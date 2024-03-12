7cd456d575110e0e7d1af44aafe3c2e865199d7f
j-Xedocs <javier.sanchez28@protonmail.com>
2024-03-11 23:41:13 -0600

    CoursesTesting Test CourseBy

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -35,12 +35,50 @@ describe('Courses Service' , () => {
 
 
     const req = httpTestingController.expectOne('/api/courses');
-    expect(req.request.method).toEqual("GET");
+
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
+    expect(req.request.method).toEqual("GET" , 'http Method Incorrect');
+
+    
     req.flush({
       payload: Object.values(COURSES)
     });
+
+    httpTestingController.verify();
   });
 
+  it('should find a course by id' , () => {
+    const id = 12
 
+    coursesService.findCourseById(id)
+      .subscribe( response => {
+        expect(response.id).toBe(id , 'Incorrect register')
+        expect(response).toBeTruthy('No courses return');
+      })
+
+    const req = httpTestingController.expectOne(`/api/courses/${id}`);
+
+    expect(req.cancelled).toBeFalsy();
+    expect(req.request.responseType).toEqual('json');
+    expect(req.request.method).toEqual("GET" , 'http Method Incorrect');
+
+    
+    req.flush({
+      id: 12,
+      titles: {
+        description: 'Angular Testing Course',
+        longDescription: 'In-depth guide to Unit Testing and E2E Testing of Angular Applications'
+      },
+      iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-testing-small.png',
+      category: 'BEGINNER',
+      seqNo: 0,
+      url: 'angular-testing-course',
+      lessonsCount: 10,
+    });
+
+
+    httpTestingController.verify();
+  })
 
 })
